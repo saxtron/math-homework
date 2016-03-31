df325ff9ef2b08c7415457ae61f7b12d915dc621
Mike Saxton <michael.saxton@cbc.ca>
2016-03-31 09:34:28 -0400

    Adding consecutive cubes script

---
+++ stewarts-casebook/consecutiveCubes.js
@@ -0,0 +1,22 @@
+// p.20 - Consecutive Cubes
+
+var perfectSquareFound = false;
+var i = 2;
+var sumOfCubes, squareOfSum;
+var j, k;
+
+while (perfectSquareFound === false) {
+   j = i + 1;
+   k = i + 2;
+   sumOfCubes = Math.pow(i, 3) + Math.pow(j, 3) + Math.pow(k, 3);
+   //console.log('Sum Of Cubes for ' + i + ', ' + j + ', ' + k + ': ' + sumOfCubes );
+
+   squareOfSum = Math.sqrt(sumOfCubes);
+   if (Math.floor(squareOfSum) === squareOfSum) {
+      console.log('The second consecutive cubes that add up to a perfect square are: ' + i + ', ' + j + ', ' + k + ' which add up to ' + sumOfCubes + ' which is a perfect square of ' + squareOfSum);
+      perfectSquareFound = true;
+   }
+
+   i++;
+}
+
