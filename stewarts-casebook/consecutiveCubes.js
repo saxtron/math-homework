819e86f1d85d076f3300ce65df24dd5f9dcd7773
Michael Saxton <saxtron@gmail.com>
2016-04-02 08:58:25 -0400

    Converting to ES6

--- stewarts-casebook/consecutiveCubes.js
+++ stewarts-casebook/consecutiveCubes.js
@@ -1,23 +1,22 @@
 // p.20 - Consecutive Cubes
+"use strict"
 
-var perfectSquareFound = false;
-var i = 1;
-var sumOfCubes, squareOfSum;
-var j, k;
+let perfectSquareFound = false;
+let i = 2;
 
 while (perfectSquareFound === false) {
-   i++;
-   j = i + 1;
-   k = i + 2;
-   sumOfCubes = Math.pow(i, 3) + Math.pow(j, 3) + Math.pow(k, 3);
-   //console.log('Sum Of Cubes for ' + i + ', ' + j + ', ' + k + ': ' + sumOfCubes );
+   const x = i;
+   const y = i + 1;
+   const z = i + 2;
 
-   squareOfSum = Math.sqrt(sumOfCubes);
+   const sumOfCubes = Math.pow(x, 3) + Math.pow(y, 3) + Math.pow(z, 3);
+   const squareOfSum = Math.sqrt(sumOfCubes);
+   //console.log(`sum of cubes for ${x}, ${y}, ${z}: ${sumOfCubes}; square of sum of cubes ${squareOfSum}`);
+
    if (Math.floor(squareOfSum) === squareOfSum) {
+      console.log(`The second consecutive cubes that add up to a perfect square are: ${i}, ${i + 1}, ${i + 2} which add up to ${sumOfCubes}, which is a perfect square of ${squareOfSum}`);
       perfectSquareFound = true;
    }
-   
+
+   i++;
 }
-
-console.log('The second consecutive cubes that add up to a perfect square are: ' + i + ', ' + j + ', ' + k + ' which add up to ' + sumOfCubes + ' which is a perfect square of ' + squareOfSum);
-
