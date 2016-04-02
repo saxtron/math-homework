d47c2dad0c215d2fdb2fdbcd91091d897967d1dc
Michael Saxton <saxtron@gmail.com>
2016-04-02 08:05:52 -0400

    Pulled the results console log out of the loop.

--- stewarts-casebook/consecutiveCubes.js
+++ stewarts-casebook/consecutiveCubes.js
@@ -1,11 +1,12 @@
 // p.20 - Consecutive Cubes
 
 var perfectSquareFound = false;
-var i = 2;
+var i = 1;
 var sumOfCubes, squareOfSum;
 var j, k;
 
 while (perfectSquareFound === false) {
+   i++;
    j = i + 1;
    k = i + 2;
    sumOfCubes = Math.pow(i, 3) + Math.pow(j, 3) + Math.pow(k, 3);
@@ -13,10 +14,10 @@ while (perfectSquareFound === false) {
 
    squareOfSum = Math.sqrt(sumOfCubes);
    if (Math.floor(squareOfSum) === squareOfSum) {
-      console.log('The second consecutive cubes that add up to a perfect square are: ' + i + ', ' + j + ', ' + k + ' which add up to ' + sumOfCubes + ' which is a perfect square of ' + squareOfSum);
       perfectSquareFound = true;
    }
-
-   i++;
+   
 }
+
+console.log('The second consecutive cubes that add up to a perfect square are: ' + i + ', ' + j + ', ' + k + ' which add up to ' + sumOfCubes + ' which is a perfect square of ' + squareOfSum);
 
