cd0fcd3ea40a241e69a2c1cd6a78a6d2670e6ec6
Michael Saxton <saxtron@gmail.com>
2016-04-08 14:47:52 -0400

    Playing around with code formatting

--- stewarts-casebook/consecutiveCubes.js
+++ stewarts-casebook/consecutiveCubes.js
@@ -2,21 +2,25 @@
 "use strict"
 
 let perfectSquareFound = false;
-let i = 2;
+let i = 2;  // start at 2, since we're finding the consectutive cubes after 1, 2, 3
+let x, y, z;  // variables to hold three consecutive numbers
+let sumOfCubes;  // the sum of the cubes of consecutive numbers 
+let squareOfSum;  // the square of the sumOfCubes
 
 while (perfectSquareFound === false) {
-   const x = i;
-   const y = i + 1;
-   const z = i + 2;
+   x = i;
+   y = i + 1;
+   z = i + 2;
 
-   const sumOfCubes = Math.pow(x, 3) + Math.pow(y, 3) + Math.pow(z, 3);
-   const squareOfSum = Math.sqrt(sumOfCubes);
+   sumOfCubes = Math.pow(x, 3) + Math.pow(y, 3) + Math.pow(z, 3);
+   squareOfSum = Math.sqrt(sumOfCubes);
    //console.log(`sum of cubes for ${x}, ${y}, ${z}: ${sumOfCubes}; square of sum of cubes ${squareOfSum}`);
 
    if (Math.floor(squareOfSum) === squareOfSum) {
-      console.log(`The second consecutive cubes that add up to a perfect square are: ${i}, ${i + 1}, ${i + 2} which add up to ${sumOfCubes}, which is a perfect square of ${squareOfSum}`);
       perfectSquareFound = true;
+   } else {
+      i++;  // check the next three numbers
    }
-
-   i++;
 }
+
+console.log(`The second consecutive cubes that add up to a perfect square are: ${x}, ${y}, ${z} which add up to ${sumOfCubes}, which is a perfect square of ${squareOfSum}`);
