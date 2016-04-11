ffabe3031d5638732e55e802898f6287e92f8cc6
Michael Saxton <saxtron@gmail.com>
2016-04-11 10:17:00 -0400

    I think I have some solutions for the quick sqaures problem.  Still need a lot of refining for this to be complete.

--- stewarts-casebook/quickSquares.js
+++ stewarts-casebook/quickSquares.js
@@ -2,60 +2,67 @@
 "use strict"
 
 // how can we ensure the ordering??  it's just a coincidence at this point.
+// add callback function parameter to findPermutations
 
-var numberBuilder = [];
+let numberBuilder = [];
+let largestPerfectSquare = [];   // decreasing numbers that include all digits
+let smallestPerfectSquare = [];
 
-findLargestSquare(987654321);
-//findSmallestSquare(123456789);
-//findSmallestSquare(1234);
+let prefectSquares = [];
 
-function findLargestSquare(largestSquare) {
+//findPerfectSquare(987654321);
+findPerfectSquare(123456789);
+//findPerfectSquare(1234);
+
+function findPerfectSquare(startingNumber) {
 	
-	numberBuilder = [];
-	findPermutations(largestSquare.toString());
+	let numberBuilder = [];
+	findPermutations(startingNumber.toString());
 
 
+	console.log(`prefect squares: ${prefectSquares}`);
 	//console.log('The largest perfect square using all the digits from 1 to 9 exactly once is: ' + largestSquare);
 }
 
 
-function findSmallestSquare(smallestSquare) {
-
-	numberBuilder = [];
-	findPermutations(smallestSquare.toString());
-
-	//console.log('The smallest perfect square using all the digits from 1 to 9 exactly once is: ' + smallestSquare);
-}
-
 
 function findPermutations(n) {
-	var i;
-	var numberToPullOut, numberToPutBackIn;  // should be the same, just want to be more explicit
-	var arrayOfNumbers = n.split('');
+	let i;
+	let arrayOfNumbers = n.split('');
 
 	for (i = 0; i < arrayOfNumbers.length; i++) {
-		numberToPullOut = arrayOfNumbers.splice(i, 1);
-		numberBuilder.push(numberToPullOut);
+		
+		const digitRemovedFromNumber = arrayOfNumbers.splice(i, 1);  // digitRemovedFromNumber and digitInsertedIntoNumber will be the same, I just want to be more explicit for now
+		numberBuilder.push(digitRemovedFromNumber);
 
 		if (arrayOfNumbers.length === 0) {
+			const potentialPerfectSquare = numberBuilder.join('');
+
+			//console.log(`Built the number: ${potentialPerfectSquare}`);
 			// test for square
-			console.log("Built the number: " + numberBuilder.join(''));
+			if (testForPerfectSquare(potentialPerfectSquare)) {
+				console.log(`I found a perfect square: ${potentialPerfectSquare}`);
+				prefectSquares.push(potentialPerfectSquare);
+				//break;
+			}
+
 		}
-		findPermutations(arrayOfNumbers.join(''));
+		findPermutations(arrayOfNumbers.join('')); // can I put this in an else?
 		//findPermutationsNonRecursive(arrayOfNumbers.join(''));
 
-		numberToPutBackIn = numberBuilder.pop();
-		arrayOfNumbers.splice(i, 0, numberToPutBackIn);
+		const digitInsertedIntoNumber = numberBuilder.pop();
+		arrayOfNumbers.splice(i, 0, digitInsertedIntoNumber);
 	}
 
 }
 
 // just trying to wrap my head around how this is working
 function findPermutationsNonRecursive(n) {
-	var arrayOfNumbers = n.split('');
+	let arrayOfNumbers = n.split('');
+	let i;
 	for (i = 0; i < arrayOfNumbers.length; i++) {
 		numberBuilder.push(arrayOfNumbers.splice(i, 1));
-		console.log("I builded: " + numberBuilder.join('') + ', with these left over: ' + arrayOfNumbers.join(''));
+		console.log(`I builded: ${numberBuilder.join('')}, with these left over: ${arrayOfNumbers.join('')}`);
 		arrayOfNumbers.splice(i, 0, numberBuilder.pop());
 	}
 
@@ -63,10 +70,10 @@ function findPermutationsNonRecursive(n) {
 
 
 
-// Test if a number, n, is a perfect square (has a whole number as a square root).  Return true if the number is a perfect square, return false otherwise
-function testForPerfectSquare(n) {
-	squareRoot = Math.sqrt(n);
-	if (Math.floor(root) === squareRoot) {
+// Test if a number, p, is a perfect square (has a whole number as a square root).  Return true if the number is a perfect square, return false otherwise
+function testForPerfectSquare(p) {
+	const squareRoot = Math.sqrt(p);
+	if (Math.floor(squareRoot) === squareRoot) {
 		return true;
 	}
 	return false;
