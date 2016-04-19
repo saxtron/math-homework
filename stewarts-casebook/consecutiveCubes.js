5dee114e299fe3550116d33b270b6935c2eb7279
Michael Saxton <saxtron@gmail.com>
2016-04-19 10:43:18 -0400

    Adding a description of the problem

--- stewarts-casebook/consecutiveCubes.js
+++ stewarts-casebook/consecutiveCubes.js
@@ -1,4 +1,5 @@
 // p.20 - Consecutive Cubes
+// The cubes of 1, 2, and 3 add up to 36, which is a perfect square of 6.  Find the next three consecutive numbers that have cubes which add up to a perfect square. 
 "use strict"
 
 let perfectSquareFound = false;
@@ -17,7 +18,7 @@ while (perfectSquareFound === false) {
 
    // check if the square is a whole number
    if (Math.floor(squareOfSum) === squareOfSum) {
-      console.log(`The second consecutive cubes that add up to a perfect square are: ${x}, ${y}, ${z} which add up to ${sumOfCubes}, which is a perfect square of ${squareOfSum}`);
+      console.log(`The second consecutive number that have cubes which add up to a perfect square are: ${x}, ${y}, ${z} - which add up to ${sumOfCubes}, which is a perfect square of ${squareOfSum}`);
       perfectSquareFound = true;
    } else {
       i++;  // check the next three numbers
