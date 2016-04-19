866bdd2fd061930dc0e84dd5319526b3e00fa457
Michael Saxton <saxtron@gmail.com>
2016-04-19 11:25:10 -0400

    converting tabs to spaces

--- stewarts-casebook/quickSquares.js
+++ stewarts-casebook/quickSquares.js
@@ -17,71 +17,71 @@ findPerfectSquare(987654321);
 //findPerfectSquare(1234);
 //carefullyIncreaseNumber();
 
-function findPerfectSquare(startingNumber) {	
-	let numberBuilder = [];
-	findPermutations(startingNumber.toString());
+function findPerfectSquare(startingNumber) {    
+    let numberBuilder = [];
+    findPermutations(startingNumber.toString());
 
-	console.log(`prefect squares: ${prefectSquares}`);
-	//console.log('The largest perfect square using all the digits from 1 to 9 exactly once is: ' + largestSquare);
+    console.log(`prefect squares: ${prefectSquares}`);
+    //console.log('The largest perfect square using all the digits from 1 to 9 exactly once is: ' + largestSquare);
 }
 
 
 function carefullyIncreaseNumber() {
-	// increase the number to the next number that contains every digit from 1 to 9 exactly once
-	// this function needs to wrap findPermutations, and get one number back at a time (need to pull some of the functionality out of findPermutations, if possible)
-	const startingNumber = 123456789;
-	let currentNumber = startingNumber;
+    // increase the number to the next number that contains every digit from 1 to 9 exactly once
+    // this function needs to wrap findPermutations, and get one number back at a time (need to pull some of the functionality out of findPermutations, if possible)
+    const startingNumber = 123456789;
+    let currentNumber = startingNumber;
 
-	let pefectSquareFound = false;
-	while (perfectSquareFound === false) {
+    let pefectSquareFound = false;
+    while (perfectSquareFound === false) {
 
-		nextNumber = findPermutations();
+        nextNumber = findPermutations();
 
-		// test nextNumber
+        // test nextNumber
 
 
-	}
+    }
 
 }
 
 function findPermutations(n) {
-	let i;
-	let arrayOfNumbers = n.split('');
+    let i;
+    let arrayOfNumbers = n.split('');
 
-	for (i = 0; i < arrayOfNumbers.length; i++) {
-		
-		const digitRemovedFromNumber = arrayOfNumbers.splice(i, 1);  // digitRemovedFromNumber and digitInsertedIntoNumber will be the same, I just want to be more explicit for now
-		numberBuilder.push(digitRemovedFromNumber);
+    for (i = 0; i < arrayOfNumbers.length; i++) {
+        
+        const digitRemovedFromNumber = arrayOfNumbers.splice(i, 1);  // digitRemovedFromNumber and digitInsertedIntoNumber will be the same, I just want to be more explicit for now
+        numberBuilder.push(digitRemovedFromNumber);
 
-		if (arrayOfNumbers.length === 0) {
-			const potentialPerfectSquare = numberBuilder.join('');
-			//console.log(`Built the number: ${potentialPerfectSquare}  - value of arrayOfNumbers.length is ${arrayOfNumbers.length}`);
-			
-			// test for prefect square
-			if (testForPerfectSquare(potentialPerfectSquare)) {
-				console.log(`I found a perfect square: ${potentialPerfectSquare}`);
-				prefectSquares.push(potentialPerfectSquare);
-				//break;
-			}
+        if (arrayOfNumbers.length === 0) {
+            const potentialPerfectSquare = numberBuilder.join('');
+            //console.log(`Built the number: ${potentialPerfectSquare}  - value of arrayOfNumbers.length is ${arrayOfNumbers.length}`);
+            
+            // test for prefect square
+            if (testForPerfectSquare(potentialPerfectSquare)) {
+                console.log(`I found a perfect square: ${potentialPerfectSquare}`);
+                prefectSquares.push(potentialPerfectSquare);
+                //break;
+            }
 
-		}
-		findPermutations(arrayOfNumbers.join('')); // can I put this in an else?
+        }
+        findPermutations(arrayOfNumbers.join('')); // can I put this in an else?
 
-		const digitInsertedIntoNumber = numberBuilder.pop();
-		arrayOfNumbers.splice(i, 0, digitInsertedIntoNumber);
-	}
+        const digitInsertedIntoNumber = numberBuilder.pop();
+        arrayOfNumbers.splice(i, 0, digitInsertedIntoNumber);
+    }
 
 }
 
 // just trying to wrap my head around how this is working
 function findPermutationsNonRecursive(n) {
-	let arrayOfNumbers = n.split('');
-	let i;
-	for (i = 0; i < arrayOfNumbers.length; i++) {
-		numberBuilder.push(arrayOfNumbers.splice(i, 1));
-		console.log(`I builded: ${numberBuilder.join('')}, with these left over: ${arrayOfNumbers.join('')}`);
-		arrayOfNumbers.splice(i, 0, numberBuilder.pop());
-	}
+    let arrayOfNumbers = n.split('');
+    let i;
+    for (i = 0; i < arrayOfNumbers.length; i++) {
+        numberBuilder.push(arrayOfNumbers.splice(i, 1));
+        console.log(`I builded: ${numberBuilder.join('')}, with these left over: ${arrayOfNumbers.join('')}`);
+        arrayOfNumbers.splice(i, 0, numberBuilder.pop());
+    }
 
 }
 
@@ -89,11 +89,11 @@ function findPermutationsNonRecursive(n) {
 
 // Test if a number, p, is a perfect square (has a whole number as a square root).  Return true if the number is a perfect square, return false otherwise
 function testForPerfectSquare(p) {
-	const squareRoot = Math.sqrt(p);
-	if (Math.floor(squareRoot) === squareRoot) {
-		return true;
-	}
-	return false;
+    const squareRoot = Math.sqrt(p);
+    if (Math.floor(squareRoot) === squareRoot) {
+        return true;
+    }
+    return false;
 
 }
 
@@ -113,23 +113,23 @@ function testForPerfectSquare(p) {
 // }
 
 // for (let i = 123456789; i < 987654321; i++) {
-// 	if (doesThisNumberHaveRepeatedDigits(i) === false) {
-// 		if (testForPerfectSquare(i)) {
-// 			console.log(`Smallest perfect square: ${i} (square of: ${Math.sqrt(i)})`);
-// 			break;
-// 		}
-// 	}
+//  if (doesThisNumberHaveRepeatedDigits(i) === false) {
+//      if (testForPerfectSquare(i)) {
+//          console.log(`Smallest perfect square: ${i} (square of: ${Math.sqrt(i)})`);
+//          break;
+//      }
+//  }
 // }
 // // 139854276 (square of: 11826)
 
 
 // for (let i = 987654321; i > 123456789; i--) {
-// 	if (doesThisNumberHaveRepeatedDigits(i) === false) {
-// 		if (testForPerfectSquare(i)) {
-// 			console.log(`Largest perfect square: ${i} (square of: ${Math.sqrt(i)})`);
-// 			break;
-// 		}
-// 	}
+//  if (doesThisNumberHaveRepeatedDigits(i) === false) {
+//      if (testForPerfectSquare(i)) {
+//          console.log(`Largest perfect square: ${i} (square of: ${Math.sqrt(i)})`);
+//          break;
+//      }
+//  }
 // }
 // 923187456 (square of: 30384)
 
